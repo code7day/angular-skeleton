4d29ef1458b7d9ebdff47a35f464807b827f87ea
Дмитрий Пацура <talk@dmtry.me>
2014-12-20 22:18:13 +0900

    Added users list

--- src/app.js
+++ src/app.js
@@ -5,8 +5,15 @@ var app = angular.module('app', [
     'ngResource'
 ]);
 
-app.factory("User", function($resource) {
-    return $resource("//phalcon-module.dmtry.me/api/users/get/:id", {id:'@id'});
+app.factory("UserService", function($resource, $http) {
+    return {
+        all: function($callback) {
+            return $http.get("//phalcon-module.dmtry.me/api/users").success($callback);
+        },
+        get: function($id, $callback) {
+            return $resource("//phalcon-module.dmtry.me/api/users/get/:id", {id:'@id'}).get({id: $id}, $callback);
+        }
+    };
 });
 
 app.config(['$routeProvider', function($routeProvider) {
@@ -25,8 +32,14 @@ app.config(['$routeProvider', function($routeProvider) {
         .otherwise({redirectTo: '/not-found'});
 }]);
 
-app.controller('userViewController', function($scope, $routeParams, User) {
-    User.get({id: $routeParams.id}, function(data) {
+app.controller('userViewController', function($scope, $routeParams, $resource, UserService) {
+    UserService.get($routeParams.id, function(data) {
         $scope.user = data.result;
+    });
+});
+
+app.controller('usersController', function($scope, $routeParams, UserService) {
+    UserService.all(function(data) {
+        $scope.users = data.result.users;
     });
 });
